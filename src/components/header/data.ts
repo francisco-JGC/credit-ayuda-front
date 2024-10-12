9d8c65a0e89ab032fdb5510365bbfa171f976bc5
francisco-JGC <jjjchico1@gmail.com>
2024-10-12 17:17:35 -0600

    feat: create data of navigations

---
+++ src/components/header/data.ts
@@ -0,0 +1,38 @@
+import {
+  BanknotesIcon,
+  CalculatorIcon,
+  DocumentChartBarIcon,
+} from "@heroicons/react/24/outline";
+
+export const reports = [
+  {
+    name: "Informe General",
+    href: "#",
+    icon: DocumentChartBarIcon,
+  },
+  {
+    name: "Informe Diario",
+    href: "#",
+    icon: DocumentChartBarIcon,
+  },
+  {
+    name: "Informe Mensual",
+    href: "#",
+    icon: DocumentChartBarIcon,
+  },
+  {
+    name: "Informe Global",
+    href: "#",
+    icon: DocumentChartBarIcon,
+  },
+  {
+    name: "Arqueo",
+    href: "#",
+    icon: CalculatorIcon,
+  },
+  {
+    name: "Caja Chica",
+    href: "#",
+    icon: BanknotesIcon,
+  },
+];
