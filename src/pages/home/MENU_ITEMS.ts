ed6cd27110478ddf40010253e633a7933e13e893
francisco-JGC <jjjchico1@gmail.com>
2024-10-12 17:51:59 -0600

    feat: create menu option to home page

---
+++ src/pages/home/MENU_ITEMS.ts
@@ -0,0 +1,41 @@
+import {
+  MapPinIcon,
+  ArrowTrendingUpIcon,
+  UserPlusIcon,
+  BanknotesIcon,
+  UsersIcon,
+  UserGroupIcon,
+} from "@heroicons/react/24/solid";
+
+export const MENU_ITEMS = [
+  {
+    path: "#",
+    label: "Mi Ruta",
+    icon: MapPinIcon,
+  },
+  {
+    path: "#",
+    label: "Préstamos",
+    icon: BanknotesIcon,
+  },
+  {
+    path: "#",
+    label: "Solicitudes",
+    icon: UserPlusIcon,
+  },
+  {
+    path: "#",
+    label: "Clientes",
+    icon: UserGroupIcon,
+  },
+  {
+    path: "#",
+    label: "Rutas",
+    icon: ArrowTrendingUpIcon,
+  },
+  {
+    path: "#",
+    label: "Usuarios",
+    icon: UsersIcon,
+  },
+];
