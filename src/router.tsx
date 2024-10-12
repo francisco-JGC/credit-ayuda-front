b09c1823ff96b47ff369d183db6bc2c5c0027228
francisco-JGC <jjjchico1@gmail.com>
2024-10-12 17:17:24 -0600

    feat: create router

---
+++ src/router.tsx
@@ -0,0 +1,16 @@
+import { Routes } from 'react-router-dom'
+
+export default function Router() {
+
+  return (
+    <Routes>
+      {/* <Route index path="/" element={<HomePage />} />
+        <Route path="/sites" element={<SitesPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/package" element={<PackagesPage />}>
+          <Route path=":destination" element={<PackagesPage />} />
+        </Route>
+        <Route path="/contact" element={<ContactPage />} /> */}
+    </Routes>
+  )
+}
