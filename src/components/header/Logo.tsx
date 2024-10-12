134d23019a50430ec97a0bf6cc227aaab5430380
francisco-JGC <jjjchico1@gmail.com>
2024-10-12 17:17:57 -0600

    feat: create logo icon component

---
+++ src/components/header/Logo.tsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom';
+import creditAyudaIcon from '../../assets/images/credit-ayuda.webp';
+
+export const Logo = () => (
+  <Link to="#" className="-m-1.5 p-1.5">
+    <span className="sr-only">Credit-Ayuda</span>
+    <img alt="Credit-Ayuda" src={creditAyudaIcon} className="h-8 w-auto" />
+  </Link>
+);
