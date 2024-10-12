57fdc994a367c3ae36f544213273fa1750bcbf35
francisco-JGC <jjjchico1@gmail.com>
2024-10-12 17:58:49 -0600

    fix: responsive

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,17 +4,22 @@ import { MENU_ITEMS } from "./MENU_ITEMS"
 export default function HomePage() {
   return (
     <div className="p-6 rounded flex items-center justify-center">
-      <div className="grid grid-cols-3  gap-16">
+      <div className="grid grid-cols-2 sm:grid-cols-3 gap-8 sm:gap-16">
         {
           MENU_ITEMS.map((item, index) => {
-            return (<Link to={item.path} className="p-4 shadow-lg rounded-lg hover:bg-gray-100 cursor-pointer flex flex-col gap-2 items-center"
-              key={index}>
-              <item.icon width={30} className="text-gray-800" />
-              <span>{item.label}</span>
-            </Link>)
+            return (
+              <Link
+                to={item.path}
+                className="p-4 shadow-lg rounded-lg hover:bg-gray-100 cursor-pointer flex flex-col gap-2 items-center"
+                key={index}
+              >
+                <item.icon width={30} className="text-gray-800" />
+                <span>{item.label}</span>
+              </Link>
+            )
           })
         }
       </div>
-    </div >
+    </div>
   )
 }
