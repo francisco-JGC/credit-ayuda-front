b788eb3d0a1c916f912d7bef0977a8233807c2d8
francisco-JGC <jjjchico1@gmail.com>
2024-10-12 17:20:33 -0600

    feat: render header with component of navigation menu

---
+++ src/components/header/index.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { Logo } from './Logo';
+import { MobileMenuButton } from './MobileMenuButton';
+import { DesktopNav } from './DesktopNav';
+import { MobileNav } from './MobileNav';
+import { UserMenu } from './UserMenu';
+
+export const Header = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <header className="bg-white w-full">
+      <nav className="flex items-center justify-between p-6 lg:px-8">
+        <Logo />
+        <div className="flex lg:hidden">
+          <MobileMenuButton onClick={() => setMobileMenuOpen(true)} />
+        </div>
+        <div className="hidden lg:flex lg:flex-grow lg:justify-center lg:gap-x-12">
+          <DesktopNav />
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <UserMenu />
+        </div>
+      </nav>
+      <MobileNav open={mobileMenuOpen} onClose={() => setMobileMenuOpen(false)} />
+    </header>
+  );
+
+};
