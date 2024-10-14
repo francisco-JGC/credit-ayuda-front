dcb8992abb2d5f42d5022ba45f3bfef1a7b96039
francisco-JGC <jjjchico1@gmail.com>
2024-10-14 10:11:39 -0600

    feat: render header with exception login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { Toaster } from 'sonner'
 import Router from './router'
-
 import { Header } from './components/header'
+import { useLocation } from 'react-router-dom'
 
 function App() {
+  const location = useLocation()
+
   return (
     <div className="mx-auto flex flex-col max-w-7xl items-center justify-between lg:px-8">
-      <Header />
+      {location.pathname !== '/login' && <Header />}
+
       <div className='w-full mt-6'>
         <Router />
       </div>
-      {/* <Footer /> */}
 
       <Toaster />
     </div>
