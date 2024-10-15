ff8854f200a0bd781077c7292b9764f281a56a0a
francisco-JGC <jjjchico1@gmail.com>
2024-10-14 20:42:36 -0600

    feat: use protecterRoute

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/home'
 import LoginPage from './pages/login'
+import ProtectedRoute from './components/protectedRoute'
 
 export default function Router() {
 
   return (
     <Routes>
-      <Route index path="/" element={<HomePage />} />
+      <Route index path="/" element={
+        <ProtectedRoute>
+          <HomePage />
+        </ProtectedRoute>
+      } />
       <Route index path="/login" element={<LoginPage />} />
     </Routes>
   )
