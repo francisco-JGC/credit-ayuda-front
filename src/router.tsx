0bbd06784a74e3afc87b8cc580367f0edda0eca2
francisco-JGC <jjjchico1@gmail.com>
2024-10-15 01:08:23 -0600

    feat: render client page

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/home'
 import LoginPage from './pages/login'
+import ClientPage from './pages/client'
 import ProtectedRoute from './components/protectedRoute'
 
 export default function Router() {
@@ -10,6 +11,12 @@ export default function Router() {
       <Route index path="/" element={
         <ProtectedRoute>
           <HomePage />
+        </ProtectedRoute>
+      } />
+
+      <Route index path="/clients" element={
+        <ProtectedRoute>
+          <ClientPage />
         </ProtectedRoute>
       } />
       <Route index path="/login" element={<LoginPage />} />
