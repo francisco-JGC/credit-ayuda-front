6662743e7f7cd5e41ceac73687c2aa024e21106d
francisco-JGC <jjjchico1@gmail.com>
2024-10-14 20:42:23 -0600

    feat: use authProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
+import AuthProvider from './components/protectedRoute/authProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider isSignedIn={false}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>,
 )
