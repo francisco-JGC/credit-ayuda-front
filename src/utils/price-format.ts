af1f43a906af6a536f3fd30edbb1d4da6dd47741
francisco-JGC <jjjchico1@gmail.com>
2024-10-15 01:09:56 -0600

    feat: update format price function to NIO

--- src/utils/price-format.ts
+++ src/utils/price-format.ts
@@ -1,5 +1,5 @@
 export const formatPrice = (price: number) => {
   if (!price) return price;
 
-  return price.toLocaleString("en-US", { style: "currency", currency: "USD" });
+  return price.toLocaleString("es-NI", { style: "currency", currency: "NIO" });
 };
