25aa8f1ba8d4d92c0302d94cab3572fca8eb7722
francisco-JGC <jjjchico1@gmail.com>
2024-10-18 01:25:16 -0600

    feat:  create route action to table route

---
+++ src/pages/routes/components/actions.tsx
@@ -0,0 +1,49 @@
+import {
+  EllipsisVertical,
+  PenIcon,
+  TrashIcon,
+} from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { IRoute } from "@/types/routes"
+
+interface IProps {
+  route: IRoute
+}
+
+export const Actions = ({ route }: IProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost">
+          <EllipsisVertical width={15} />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <PenIcon className="mr-2 h-4 w-4" />
+            <span>Modificar Ruta</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className='text-red-500'>
+          <TrashIcon className="mr-2 h-4 w-4" />
+          <span>Eliminar Ruta</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
