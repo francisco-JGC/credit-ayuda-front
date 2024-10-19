83d7b87a955b3aa760f7245e3938b60035f71750
francisco-JGC <jjjchico1@gmail.com>
2024-10-18 22:56:25 -0600

    chore: formatter

--- src/types/clients.ts
+++ src/types/clients.ts
@@ -1,20 +1,20 @@
 export interface IClientTable {
-  id: number;
-  name: string;
-  phone: string;
-  address: string;
-  currentDebt: number;
-  route: string;
-  loanStatus: "approved" | "pending" | "paid";
+  id: number
+  name: string
+  phone: string
+  address: string
+  currentDebt: number
+  route: string
+  loanStatus: 'approved' | 'pending' | 'paid'
 }
 
 export interface ICreateClient {
-  name: string;
-  dni: string;
-  primary_phone: string;
-  secondary_phone?: string;
-  primary_address: string;
-  secondary_address?: string;
-  business_type: string;
-  route_name: string;
+  name: string
+  dni: string
+  primary_phone: string
+  secondary_phone?: string
+  primary_address: string
+  secondary_address?: string
+  business_type: string
+  route_name: string
 }
