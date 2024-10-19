2bced74f0c4e559df9f35494cb7d917bdd90b227
francisco-JGC <jjjchico1@gmail.com>
2024-10-18 22:54:57 -0600

    feat:  create form loan details component

---
+++ src/pages/loan/createLoanPage/components/formLoanDetails.tsx
@@ -0,0 +1,55 @@
+import { Input } from "@/components/ui/input"
+import { ICreateLoan } from "@/types/loans"
+import { FrequencyPayment } from "./frequencyPayment"
+import { PaymentTerm } from "./paymentTerm"
+
+
+interface IProps {
+  formValues: ICreateLoan
+  handleInputChange: (e: any) => void
+}
+
+export const FormLoanDetails = ({ formValues, handleInputChange }: IProps) => {
+  return (
+    <div>
+      <div className="px-4 sm:px-0">
+        <h3 className="text-base font-semibold leading-7 text-gray-900">Detalles de aplicacion de prestamo</h3>
+        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Por favor llene los siguientes campos</p>
+      </div>
+      <div className="mt-6 border-t border-gray-100">
+        <dl className="divide-y divide-gray-100">
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Monto a desembolsar</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <Input type="number" placeholder="0" value={formValues.amount} name="amount" onChange={handleInputChange} />
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Tasa de interes (%)</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <Input type="number" placeholder="0" value={formValues.interest_rate} name="interest_rate" onChange={handleInputChange} />
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Frecuencia de pago</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <FrequencyPayment handleSetFrequency={() => { }} />
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Plazo</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <PaymentTerm handleSetFrequency={() => { }} frequency="yearly" />
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Fecha de inicio</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <Input type="date" value={formValues.load_date} name="load_date" onChange={handleInputChange} />
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  )
+}
