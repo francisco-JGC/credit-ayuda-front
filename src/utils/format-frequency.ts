01708b26b33821c45872f81843ade5e8f5c28ee8
francisco-JGC <jjjchico1@gmail.com>
2024-10-20 22:42:45 -0600

    chore: rename frequency

--- src/utils/format-frequency.ts
+++ src/utils/format-frequency.ts
@@ -2,7 +2,7 @@ export const formatFrequency = (
   frequency: 'daily' | 'weekly' | 'biweekly' | 'monthly' | 'yearly',
 ): string => {
   const frequencyMap: Record<typeof frequency, string> = {
-    daily: 'Diaria',
+    daily: 'Diario',
     weekly: 'Semanal',
     biweekly: 'Quincenal',
     monthly: 'Mensual',
