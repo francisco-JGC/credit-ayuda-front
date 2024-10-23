09784bd1b4c1767a474ad260be1da5f93eb97c5e
francisco-JGC <jjjchico1@gmail.com>
2024-10-22 23:12:22 -0600

    fix

--- src/utils/fetch-data.ts
+++ src/utils/fetch-data.ts
@@ -1,22 +1,30 @@
-import Cookies from "js-cookie";
+import Cookies from 'js-cookie'
 
 interface FetchDataParams<T> {
-  url: string;
-  method: "GET" | "POST" | "PUT" | "DELETE";
-  data?: T;
-  useToken?: boolean;
+  url: string
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE'
+  data?: T
+  useToken?: boolean
 }
 
 export interface IHandleResponse<T = unknown> {
-  message?: string;
-  data?: T;
-  success: boolean;
+  message?: string
+  data?: T
+  success: boolean
 }
 
 export interface IPagination {
-  filter?: string;
-  page: number;
-  limit: number;
+  filter?: string
+  page: number
+  limit: number
+}
+
+export interface IPaginationResponse<T = unknown> {
+  page: number
+  limit: number
+  data?: T
+  total_data: number
+  total_page: number
 }
 
 export async function fetchData<TData = any>({
@@ -26,36 +34,36 @@ export async function fetchData<TData = any>({
   useToken = false,
 }: FetchDataParams<TData>): Promise<IHandleResponse> {
   const headers: HeadersInit = {
-    "Content-Type": "application/json",
-  };
+    'Content-Type': 'application/json',
+  }
 
   if (useToken) {
-    const token = Cookies.get("token");
+    const token = Cookies.get('token')
     if (token) {
-      headers["Authorization"] = `Bearer ${token}`;
+      headers['Authorization'] = `Bearer ${token}`
     } else {
-      throw new Error("Token no encontrado");
+      throw new Error('Token no encontrado')
     }
   }
 
   const options: RequestInit = {
     method,
     headers,
-  };
+  }
 
-  if (method !== "GET" && data) {
-    options.body = JSON.stringify(data);
+  if (method !== 'GET' && data) {
+    options.body = JSON.stringify(data)
   }
 
   try {
     const response = await fetch(
       `${import.meta.env.VITE_BASE_URL}${url}`,
-      options
-    );
+      options,
+    )
 
-    return await response.json();
+    return await response.json()
   } catch (error: any) {
-    console.error("Error en la solicitud:", error.message);
-    throw error;
+    console.error('Error en la solicitud:', error.message)
+    throw error
   }
 }
