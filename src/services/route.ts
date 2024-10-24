e9498aba91dc4962b169be69b184f36caccceaa4
francisco-JGC <jjjchico1@gmail.com>
2024-10-23 21:41:11 -0600

    fixBug: use token

--- src/services/route.ts
+++ src/services/route.ts
@@ -1,21 +1,33 @@
-import { ICreateRoute } from "@/types/routes";
-import { fetchData, IHandleResponse } from "@/utils/fetch-data";
+import { ICreateRoute } from '@/types/routes'
+import { fetchData, IHandleResponse, IPagination } from '@/utils/fetch-data'
 
 export const createRoute = async (
-  route: ICreateRoute
+  route: ICreateRoute,
 ): Promise<IHandleResponse> => {
   return await fetchData({
-    url: "/route/create",
+    url: '/route/create',
     data: route,
-    method: "POST",
+    method: 'POST',
     useToken: true,
-  });
-};
+  })
+}
 
 export const getAllRoutes = async (): Promise<IHandleResponse> => {
   return await fetchData({
-    url: "/route",
-    method: "GET",
+    url: '/route',
+    method: 'GET',
     useToken: true,
-  });
-};
+  })
+}
+
+export const getPaginationRoutes = async ({
+  filter,
+  page,
+  limit,
+}: IPagination): Promise<IHandleResponse> => {
+  return await fetchData({
+    url: `/route/${page}/${limit}/${filter}`,
+    method: 'GET',
+    useToken: true,
+  })
+}
