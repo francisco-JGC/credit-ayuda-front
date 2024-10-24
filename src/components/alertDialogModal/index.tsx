89c7513bc88ce8c7eda4fdbc662d18c8259ce71f
francisco-JGC <jjjchico1@gmail.com>
2024-10-23 23:13:42 -0600

    feat: create alert dialog  modal component

---
+++ src/components/alertDialogModal/index.tsx
@@ -0,0 +1,42 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { ReactNode } from "react"
+
+interface IProps {
+  title: string
+  subtitle: string
+  onConfirm: () => void
+  children: ReactNode
+}
+
+export const AlertDialogModal = ({ onConfirm, title, subtitle, children }: IProps) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger className="w-full">{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>
+            {subtitle}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={onConfirm}
+          >Confirmar</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+
+  )
+}
