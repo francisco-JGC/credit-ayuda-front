883317e67b4d30d6a5308942aec3d5b8ea8328b9
francisco-JGC <jjjchico1@gmail.com>
2024-10-25 00:15:37 -0600

    feat: render all loan information

--- src/pages/loan/detailsLoanPage/index.tsx
+++ src/pages/loan/detailsLoanPage/index.tsx
@@ -1,25 +1,45 @@
 import { LayuotPage } from "@/components/layuotPage";
 import { getLoanById } from "@/services/loan";
-import { useEffect } from "react";
+import { ILoan } from "@/types/loans";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { PreviewClientInfo } from "../createLoanPage/components/previewClientInfo";
+import { LoanDetails } from "./components/loanDetail";
+import { PaymentSchedule } from "./components/paymentSchedule";
 
 export default function DetailsLoanPage() {
-
+  const [loan, setLoan] = useState<ILoan>({} as ILoan)
   const params = useParams()
 
   useEffect(() => {
     getLoanById(Number(params.id))
       .then((response) => {
         if (response.success) {
-          console.log(response.data)
+          setLoan(response.data as ILoan)
         }
       })
   }, [params.id])
 
   return (
     <LayuotPage title="Detalles del prestamo" description="">
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-4">
+        <PreviewClientInfo client={{
+          ...loan.client,
+          route_name: loan?.client?.route?.name || ''
+        }}
+          isLoading={false}
+        />
 
+        <LoanDetails
+          frequency={loan?.payment_plan?.frequency}
+          amount={loan?.amount}
+          total_recovered={loan?.total_recovered}
+          interest_rate={loan?.interest_rate}
+          loan_date={loan?.loan_date}
+          created_at={loan.created_at}
+        />
+
+        <PaymentSchedule payment_plan={loan?.payment_plan} />
       </div>
     </LayuotPage>
   )
