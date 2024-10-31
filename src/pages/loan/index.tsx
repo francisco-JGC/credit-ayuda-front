d35e63fe92e5340c3fd9eb1d03c5d8d88dbe5b09
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-10-31 14:43:41 -0600

    feat: integrate pagination and enhance loan display on LoanPage feat: removed usage of search by dni from use loan filters

--- src/pages/loan/index.tsx
+++ src/pages/loan/index.tsx
@@ -7,16 +7,22 @@ import { Search } from 'lucide-react'
 import { Link } from 'react-router-dom'
 import { LoansTable } from './components/loans-table'
 import { useLoans } from './hooks/use-loans'
+import { LoansPagination } from './components/loans-pagination'
 
 export default function LoanPage() {
-  const { data: loans = [], isLoading, error } = useLoans()
   const {
-    filteredLoans,
-    dniQuery,
+    loans,
+    isLoading,
+    error,
+    currentPage,
+    totalPages,
+    goToPage,
     searchByDni,
-    filterByFrequency,
-    filterByRoute,
-  } = useLoanFilters({ loans })
+    totalLoans,
+  } = useLoans({ limit: 5 })
+  const { filteredLoans, filterByFrequency, filterByRoute } = useLoanFilters({
+    loans,
+  })
 
   return (
     <div className="flex flex-col gap-8 p-4 md:p-6 lg:p-8">
@@ -24,7 +30,6 @@ export default function LoanPage() {
         <Link to={'/loans/create'}>
           <Button className="w-full md:w-auto">Nuevo Prestamo</Button>
         </Link>
-
         <div className="flex flex-col md:flex-row justify-between gap-4 w-full md:w-auto">
           <div className="relative w-full md:w-[350px]">
             <Search
@@ -36,7 +41,6 @@ export default function LoanPage() {
               className="pl-10 w-full"
               name="search_dni"
               onChange={(e) => searchByDni(e.target.value)}
-              value={dniQuery}
             />
           </div>
           <div className="mt-2 md:mt-0 flex gap-2">
@@ -46,8 +50,24 @@ export default function LoanPage() {
         </div>
       </div>
 
-      <div className="mt-6">
+      <div>
+        <div className="mb-2">
+          <span className="text-sm text-muted-foreground">
+            Mostrando {loans.length} de {totalLoans} préstamos.
+          </span>
+        </div>
         <LoansTable isLoading={isLoading} loans={filteredLoans} error={error} />
+      </div>
+      <div className="flex justify-end">
+        <div>
+          {!isLoading && error == null && totalPages > 0 && (
+            <LoansPagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              goToPage={goToPage}
+            />
+          )}
+        </div>
       </div>
     </div>
   )
