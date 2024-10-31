1382bf16b40ae4310f653d4b74bdcd3e9611013f
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-10-30 23:59:34 -0600

    feat: integrate React Query for data fetching in main application

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
 import './index.css'
 import AuthProvider from './components/protectedRoute/authProvider.tsx'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider isSignedIn={false}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider isSignedIn={false}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
