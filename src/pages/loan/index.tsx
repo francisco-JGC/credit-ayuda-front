3bd84fd8811dea08c85fbe462505f35e2a1f79de
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-10-31 00:01:37 -0600

    refactor: update LoanPage to use custom hook for fetching loans and improve loading state handling

--- src/pages/loan/index.tsx
+++ src/pages/loan/index.tsx
@@ -2,17 +2,14 @@ import { FilterFrequency } from '@/components/filterTables/filterFrequency'
 import { FilterRoute } from '@/components/filterTables/filterRoute'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { useLoanFilters } from '@/hooks/use-loan-filters'
-import { getPaginationLoans } from '@/services/loan'
-import { ILoanTable } from '@/types/loans'
-import { IPaginationResponse } from '@/utils/fetch-data'
+import { useLoanFilters } from '@/pages/loan/hooks/use-loan-filters'
 import { Search } from 'lucide-react'
-import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { LoansTable } from './components/loans-table'
+import { useLoans } from './hooks/use-loans'
 
 export default function LoanPage() {
-  const [loans, setLoans] = useState<ILoanTable[]>([])
+  const { data: loans = [], isLoading, error } = useLoans()
   const {
     filteredLoans,
     dniQuery,
@@ -20,15 +17,6 @@ export default function LoanPage() {
     filterByFrequency,
     filterByRoute,
   } = useLoanFilters({ loans })
-
-  useEffect(() => {
-    getPaginationLoans({ page: 1, limit: 20, filter: '' }).then((response) => {
-      if (response.success) {
-        const { data } = response.data as IPaginationResponse<ILoanTable[]>
-        setLoans(data ?? [])
-      }
-    })
-  }, [])
 
   return (
     <div className="flex flex-col gap-8 p-4 md:p-6 lg:p-8">
@@ -51,16 +39,15 @@ export default function LoanPage() {
               value={dniQuery}
             />
           </div>
-
           <div className="mt-2 md:mt-0 flex gap-2">
             <FilterFrequency onChangeFrequency={filterByFrequency} />
-            <FilterRoute handleSetRouteFilter={filterByRoute} />
+            <FilterRoute onChangeRoute={filterByRoute} />
           </div>
         </div>
       </div>
 
-      <div className="overflow-x-auto mt-6">
-        <LoansTable loans={filteredLoans} />
+      <div className="mt-6">
+        <LoansTable isLoading={isLoading} loans={filteredLoans} error={error} />
       </div>
     </div>
   )
