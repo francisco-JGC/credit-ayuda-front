e060b6ad6db13727215b9e373be0be05f154188c
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-10-31 19:53:56 -0600

    feat: add getLoans function with pagination and filter options for loans

--- src/services/loan.ts
+++ src/services/loan.ts
@@ -1,5 +1,11 @@
-import { ICreateLoan } from '@/types/loans'
-import { fetchData, IHandleResponse, IPagination } from '@/utils/fetch-data'
+import { ICreateLoan, ILoan } from '@/types/loans'
+import {
+  fetchData,
+  IHandleResponse,
+  IPagination,
+  IPaginationResponse,
+} from '@/utils/fetch-data'
+import Cookies from 'js-cookie'
 
 export const createLoan = async (
   loan: ICreateLoan,
@@ -22,6 +28,48 @@ export const getPaginationLoans = async ({
     method: 'GET',
     useToken: true,
   })
+}
+
+interface IGetLoans {
+  page: number
+  limit: number
+  dni?: string
+  status?: string
+  frequency?: string
+  route?: string
+}
+
+export async function getLoans({
+  page,
+  limit,
+  dni = '',
+  status,
+  frequency,
+  route,
+}: IGetLoans) {
+  const token = Cookies.get('token')
+  const url = new URL(import.meta.env.VITE_BASE_URL)
+  url.pathname += `/loan/full/${page}/${limit}/${dni}`
+  if (status) url.searchParams.append('status', status)
+  if (frequency) url.searchParams.append('frequency', frequency)
+  if (route) url.searchParams.append('route', route)
+
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+  if (!response.ok) {
+    throw new Error('Error al obtener los préstamos')
+  }
+
+  const { success, data, message } = (await response.json()) as IHandleResponse<
+    IPaginationResponse<ILoan[]>
+  >
+  if (!success) {
+    throw new Error(message)
+  }
+  return data
 }
 
 export const getLoanById = async (id: number): Promise<IHandleResponse> => {
