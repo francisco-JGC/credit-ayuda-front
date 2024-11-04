3f4e729f75b424fb39b78388f201ccf85cc011aa
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-11-03 19:08:16 -0600

    feat: create PaymentsPage component with loan and client details

---
+++ src/pages/payments/page.tsx
@@ -0,0 +1,40 @@
+import { useParams } from 'react-router-dom'
+import { ClientDetails } from './components/client-details'
+import { LoanDetails } from './components/loan-details'
+import { useLoanDetails } from './hook/use-loan-details'
+import { PaymentsTable } from './components/payments-table'
+import { Button } from '@/components/ui/button'
+
+export function PaymentsPage() {
+  const { id } = useParams()
+  const { loan } = useLoanDetails({ id: Number(id) })
+
+  if (loan == null) {
+    return null
+  }
+
+  return (
+    <div className="">
+      <div className="flex justify-between">
+        <div className="">
+          <h2 className="text-2xl font-medium">Abonos</h2>
+          <p className="text-sm text-muted-foreground">Préstamo #{id}</p>
+        </div>
+        <div>
+          <Button>Agregar abono</Button>
+        </div>
+      </div>
+      <div className="grid grid-cols-3 gap-4 mt-4">
+        <div className="">
+          <ClientDetails loan={loan} />
+        </div>
+        <div className="col-span-2 row-span-2">
+          <PaymentsTable loan={loan} />
+        </div>
+        <div className="">
+          <LoanDetails loan={loan} />
+        </div>
+      </div>
+    </div>
+  )
+}
