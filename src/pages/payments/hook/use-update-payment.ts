f65c1257b1d5c0e53f39207544c39fd88d72d460
fabimont28 <fabian.montoya2410@gmail.com>
2024-11-04 21:54:41 -0600

    feat: enhance useUpdatePayment hook to invalidate loan queries on success and use async mutation

--- src/pages/payments/hook/use-update-payment.ts
+++ src/pages/payments/hook/use-update-payment.ts
@@ -1,13 +1,17 @@
 import { updatePayment } from '@/services/payments'
 import { IPaymentSchedule } from '@/types/loans'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
-export function useUpdatePayment() {
-  const { mutate, isPending, error, isSuccess } = useMutation({
+export function useUpdatePayment({ loanId }: { loanId: number }) {
+  const queryClient = useQueryClient()
+  const { mutateAsync, isPending, error, isSuccess } = useMutation({
     mutationFn: async (newPaymentSchedule: IPaymentSchedule) => {
       return await updatePayment(newPaymentSchedule)
     },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['loan', loanId] })
+    },
   })
 
-  return { update: mutate, isPending, error, isSuccess }
+  return { update: mutateAsync, isPending, error, isSuccess }
 }
