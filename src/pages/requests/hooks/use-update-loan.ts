a66d597bb391c65b47e9d5fc824a665d4fd26135
fabimont28 <fabian.montoya2410@gmail.com>
2024-11-07 16:14:08 -0600

    feat: implement custom hook for updating loans with mutation handling

---
+++ src/pages/requests/hooks/use-update-loan.ts
@@ -0,0 +1,25 @@
+import { updateLoan } from '@/services/loan'
+import { ILoan } from '@/types/loans'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+export function useUpdateLoan() {
+  const queryClient = useQueryClient()
+  const { mutateAsync, isPending, error } = useMutation({
+    mutationFn: async (loan: ILoan) => {
+      const response = await updateLoan(loan)
+      if (!response.success) {
+        throw new Error(response.message)
+      }
+      return response.data
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['loans'] })
+    },
+  })
+
+  return {
+    update: mutateAsync,
+    isLoading: isPending,
+    error,
+  }
+}
