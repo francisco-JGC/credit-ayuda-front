b2475edf56a69e753701372f226f759450dd4c4d
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-11-12 17:54:44 -0600

    feat: extend user types to include roles and additional user properties

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,5 +1,23 @@
+import { IRoute } from './routes'
+
 export type UserLoggedin = {
-  username: string;
-  token: string;
-  role: string;
-};
+  username: string
+  token: string
+  role: string
+}
+
+export interface Roles {
+  id: number
+  name: string
+  label: string
+  description: string
+  created_at: string
+}
+
+export type User = {
+  id: number
+  username: string
+  roles: Roles[]
+  route?: IRoute
+  created_at: string
+}
