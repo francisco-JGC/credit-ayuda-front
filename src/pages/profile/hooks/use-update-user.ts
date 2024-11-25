66a86c19492b3d074a2c67096b1dbaaba074c127
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-11-25 17:27:58 -0600

    feat: add query invalidation on user update success

--- src/pages/profile/hooks/use-update-user.ts
+++ src/pages/profile/hooks/use-update-user.ts
@@ -1,11 +1,15 @@
 import { updateUser } from '@/services/user'
 import { User } from '@/types/user'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 export function useUpdateUser() {
+  const queryClient = useQueryClient()
   const { mutateAsync, isPending } = useMutation({
     mutationFn: async (user: User) => {
       return await updateUser(user)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['users'] })
     },
   })
 
