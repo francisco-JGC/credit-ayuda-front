4fca3e9837c599452928a6802694ed71546b7959
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-11-26 17:07:10 -0600

    feat: display total number of requests on RequestsPage

--- src/pages/requests/pages/index.tsx
+++ src/pages/requests/pages/index.tsx
@@ -3,13 +3,15 @@ import { RequestsActions } from '../components/requests-actions'
 import { useRequests } from '../hooks/use-requests'
 
 export function RequestsPage() {
-  const { requests, error, isLoading } = useRequests()
+  const { requests, error, isLoading, totalRequests } = useRequests()
 
   return (
     <div>
       <div className="">
         <h1 className="text-2xl font-medium">Solicitudes</h1>
-        <p className="text-sm text-muted-foreground">10 solicitudes.</p>
+        <p className="text-sm text-muted-foreground">
+          {totalRequests} solicitud/es.
+        </p>
       </div>
       <div className="mt-4">
         <LoansTable
