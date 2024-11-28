316f09d5738fb3565eacd2deea3f0149e7654567
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-11-27 18:01:07 -0600

    feat: add getPayment function to fetch payment details by ID

--- src/services/payments.ts
+++ src/services/payments.ts
@@ -1,4 +1,4 @@
-import { IPaymentSchedule } from '@/types/loans'
+import { IPaymentSchedule, IPaymentScheduleDetails } from '@/types/loans'
 import { IHandleResponse } from '@/utils/fetch-data'
 import Cookies from 'js-cookie'
 
@@ -20,6 +20,30 @@ export async function updatePayment(paymentSchedule: IPaymentSchedule) {
   }
 
   const data = (await response.json()) as IHandleResponse<IPaymentSchedule>
+
+  if (!data.success) {
+    throw new Error(data.message)
+  }
+
+  return data.data
+}
+
+export async function getPayment(id: number) {
+  const token = Cookies.get('token')
+  const url = new URL(import.meta.env.VITE_BASE_URL)
+  url.pathname = `/api/payments/${id}`
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  if (!response.ok) {
+    throw new Error('Error fetching payment')
+  }
+
+  const data =
+    (await response.json()) as IHandleResponse<IPaymentScheduleDetails>
 
   if (!data.success) {
     throw new Error(data.message)
