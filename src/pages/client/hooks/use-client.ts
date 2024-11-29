8a716dd4b2852761b4fdb9e7e23082589fee46ed
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-11-29 17:24:57 -0600

    feat: add client deletion functionality with mutation and query invalidation

--- src/pages/client/hooks/use-client.ts
+++ src/pages/client/hooks/use-client.ts
@@ -1,5 +1,5 @@
-import { getClientById } from '@/services/client'
-import { useQuery } from '@tanstack/react-query'
+import { deleteClientById, getClientById } from '@/services/client'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 export function useClient({ id }: { id: number }) {
   const { data, isFetching, error } = useQuery({
@@ -17,6 +17,27 @@ export function useClient({ id }: { id: number }) {
   return {
     client: data,
     isLoading: isFetching,
+    error,
+  }
+}
+
+export function useDeleteClient() {
+  const queryClient = useQueryClient()
+
+  const { mutateAsync, error, isPending } = useMutation({
+    mutationFn: async (id: number) => {
+      deleteClientById(id)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['clients'],
+      })
+    },
+  })
+
+  return {
+    deleteClient: mutateAsync,
+    isLoading: isPending,
     error,
   }
 }
