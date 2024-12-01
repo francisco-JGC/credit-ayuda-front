4ffffbd9687faf67e194182bc8201f860e948d72
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-11-30 21:54:45 -0600

    feat: ensure price is a number before formatting for consistent output

--- src/utils/price-format.ts
+++ src/utils/price-format.ts
@@ -1,5 +1,6 @@
 export const formatPrice = (price: number) => {
   if (price == null) return price
+  price = Number(price)
 
   return price.toLocaleString('es-NI', { style: 'currency', currency: 'NIO' })
 }
