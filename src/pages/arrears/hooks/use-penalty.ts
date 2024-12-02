075aa6e263b5a25ff142f29f68daeed6238aae1a
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-12-02 17:12:49 -0600

    feat: add hooks for penalty management including fetching and adding penalty payments

--- src/pages/arrears/hooks/use-penalty.ts
+++ src/pages/arrears/hooks/use-penalty.ts
@@ -1,6 +1,13 @@
-import { createPenaltyPlan } from '@/services/penalty'
-import { ICreatePenaltyPlan } from '@/types/loans'
-import { useMutation } from '@tanstack/react-query'
+import {
+  addPenaltyPayment,
+  createPenaltyPlan,
+  getPenalty,
+} from '@/services/penalty'
+import {
+  ICreatePenaltyPlan,
+  IPenaltyPaymentScheduleCreate,
+} from '@/types/loans'
+import { useMutation, useQuery } from '@tanstack/react-query'
 
 export function useCreatePenalty() {
   const {
@@ -19,5 +26,44 @@ export function useCreatePenalty() {
     error,
     isPending,
     createPlan,
+  }
+}
+
+export function usePenalty({ id }: { id: number }) {
+  const {
+    data: penaltyPlan,
+    isFetching: isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['penalty', id],
+    queryFn: async () => {
+      return await getPenalty(id)
+    },
+  })
+
+  return {
+    penaltyPlan,
+    isLoading,
+    error,
+  }
+}
+
+export function useAddPenaltyPayment({ id }: { id: number }) {
+  const {
+    data: penaltyPayment,
+    isPending,
+    error,
+    mutateAsync: addPayment,
+  } = useMutation({
+    mutationFn: async (penaltyPayment: IPenaltyPaymentScheduleCreate) => {
+      return await addPenaltyPayment(id, penaltyPayment)
+    },
+  })
+
+  return {
+    penaltyPayment,
+    isPending,
+    error,
+    addPayment,
   }
 }
