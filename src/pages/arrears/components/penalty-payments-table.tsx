8a4389f30ed62db38f6526fae41d64420dc86599
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-12-02 17:12:40 -0600

    feat: add PenaltyPaymentsTable component for displaying penalty payment schedules

---
+++ src/pages/arrears/components/penalty-payments-table.tsx
@@ -0,0 +1,55 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { IPenaltyPaymentSchedule } from '@/types/loans'
+import { penaltyStatusMap } from '@/utils/contants'
+import { formatDateLong } from '@/utils/date-format'
+import { formatPrice } from '@/utils/price-format'
+
+interface PenaltyPaymentsTableProps {
+  penaltyPayments: IPenaltyPaymentSchedule[]
+}
+
+export function PenaltyPaymentsTable({
+  penaltyPayments,
+}: PenaltyPaymentsTableProps) {
+  return (
+    <div className="h-[220px] overflow-y-auto">
+      <Table>
+        <TableHeader className="bg-gray-100">
+          <TableRow className="[&>th]:px-4 [&>th]:text-xs [&>th]:sticky [&>th]:z-10 [&>th]:top-0">
+            <TableHead>ID</TableHead>
+            <TableHead>Fecha de pago</TableHead>
+            <TableHead>Monto pagado</TableHead>
+            <TableHead>Monto restante</TableHead>
+            <TableHead>Estado</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {penaltyPayments.map((penaltyPayment) => (
+            <TableRow key={penaltyPayment.id} className="[&>td]:px-4">
+              <TableCell>#{penaltyPayment.id}</TableCell>
+              <TableCell>{formatDateLong(penaltyPayment.dueDate)}</TableCell>
+              <TableCell>{formatPrice(penaltyPayment.amount_paid)}</TableCell>
+              <TableCell>{formatPrice(penaltyPayment.amount_due)}</TableCell>
+              <TableCell>{penaltyStatusMap[penaltyPayment.status]}</TableCell>
+            </TableRow>
+          ))}
+
+          {penaltyPayments.length === 0 && (
+            <TableRow>
+              <TableCell colSpan={5} className="text-center">
+                No hay pagos de mora
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  )
+}
