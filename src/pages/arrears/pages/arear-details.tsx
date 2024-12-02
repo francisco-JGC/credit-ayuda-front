eedec47d2921fa2ae376c78b87fd350b6d1357fe
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-12-02 17:12:56 -0600

    feat: add ArrearDetailsPage component for displaying detailed penalty information and payment management

---
+++ src/pages/arrears/pages/arear-details.tsx
@@ -0,0 +1,121 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { penaltyStatusMap } from '@/utils/contants'
+import { formatDateLong } from '@/utils/date-format'
+import { formatPrice } from '@/utils/price-format'
+import { useParams } from 'react-router-dom'
+import { AddPenaltyPayment } from '../components/add-penalty-payment'
+import { PenaltyPaymentsTable } from '../components/penalty-payments-table'
+import { usePenalty } from '../hooks/use-penalty'
+
+export function ArrearDetailsPage() {
+  const { id } = useParams<{ id: string }>()
+  const { penaltyPlan } = usePenalty({ id: Number(id) })
+
+  const totalPaid =
+    penaltyPlan?.penalty_payment_schedules.reduce(
+      (acc, schedule) => acc + Number(schedule.amount_paid),
+      0,
+    ) ?? 0
+
+  const paymentsDone = penaltyPlan?.penalty_payment_schedules.filter(
+    (schedule) => schedule.status === 'paid',
+  ).length
+
+  return (
+    <div>
+      <div className="flex justify-between">
+        <div>
+          <h2 className="text-2xl font-medium">Mora #{id}</h2>
+          <p className="text-sm text-muted-foreground">
+            Préstamo #{penaltyPlan?.loan.id}.
+          </p>
+        </div>
+        <div>
+          {penaltyPlan != null && (
+            <AddPenaltyPayment penaltyPlan={penaltyPlan} />
+          )}
+        </div>
+      </div>
+      <Card className="h-full shadow-sm mt-4 rounded-sm">
+        <CardHeader>
+          <CardTitle>Detalles de la mora</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="grid grid-cols-4 w-full gap-x-2 gap-y-6">
+            <p className="inline-flex flex-col">
+              <span className="text-sm text-muted-foreground">Cliente: </span>
+              <span className="font-medium">
+                {penaltyPlan?.loan?.client.name}{' '}
+                <span className="text-sm text-muted-foreground">
+                  ({penaltyPlan?.loan?.client.dni})
+                </span>
+              </span>
+            </p>
+            <p className="inline-flex flex-col">
+              <span className="text-sm text-muted-foreground">
+                Fecha de creación:{' '}
+              </span>
+              <span className="font-medium">
+                {penaltyPlan?.created_at != null &&
+                  formatDateLong(penaltyPlan?.created_at)}
+              </span>
+            </p>
+            <p className="inline-flex flex-col">
+              <span className="text-sm text-muted-foreground">Estado:</span>
+              <span className="font-medium">
+                {penaltyPlan?.status != null &&
+                  penaltyStatusMap[penaltyPlan?.status]}
+              </span>
+            </p>
+            <p className="inline-flex flex-col">
+              <span className="text-sm text-muted-foreground">
+                Monto total de la mora:
+              </span>
+              <span className="font-medium">
+                {formatPrice(penaltyPlan?.total_penalty_amount ?? 0)}
+              </span>
+            </p>
+            <p className="inline-flex flex-col">
+              <span className="text-sm text-muted-foreground">
+                Monto pendiente:
+              </span>
+              <span className="font-medium">
+                {formatPrice(penaltyPlan?.current_penalty_amount ?? 0)}
+              </span>
+            </p>
+            <p className="inline-flex flex-col">
+              <span className="text-sm text-muted-foreground">
+                Interés por mora:
+              </span>
+              <span className="font-medium">
+                {penaltyPlan?.interest_rate ?? 0}%
+              </span>
+            </p>
+            <p className="inline-flex flex-col">
+              <span className="text-sm text-muted-foreground">
+                Total pagado:
+              </span>
+              <span className="font-medium">{formatPrice(totalPaid)}</span>
+            </p>
+            <p className="inline-flex flex-col">
+              <span className="text-sm text-muted-foreground">
+                Pagos realizados:
+              </span>
+              <span className="font-medium">{paymentsDone}</span>
+            </p>
+          </div>
+        </CardContent>
+      </Card>
+      <Card className="h-full shadow-sm mt-4 rounded-sm">
+        <CardHeader>
+          <CardTitle>Pagos de la mora</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <PenaltyPaymentsTable
+            penaltyPayments={penaltyPlan?.penalty_payment_schedules ?? []}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
