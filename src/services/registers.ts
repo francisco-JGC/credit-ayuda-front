8ee0ce917dd33524b501914b7bd81c74cfdc5a6e
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-12-16 00:25:30 -0600

    feat: add service functions for fetching and creating registers

---
+++ src/services/registers.ts
@@ -0,0 +1,35 @@
+import { CreateRegister, Register } from '@/types/registers'
+import { fetchData } from '@/utils/fetch-data'
+
+export async function getAllRegisters() {
+  const url = '/registers'
+
+  const response = await fetchData<Register[]>({
+    url,
+    method: 'GET',
+    useToken: true,
+  })
+
+  if (!response.success) {
+    throw new Error(response.message)
+  }
+
+  return response.data
+}
+
+export async function createRegister(register: CreateRegister) {
+  const url = '/registers/create'
+
+  const response = await fetchData<Register>({
+    url,
+    method: 'POST',
+    useToken: true,
+    data: register,
+  })
+
+  if (!response.success) {
+    throw new Error(response.message)
+  }
+
+  return response.data
+}
