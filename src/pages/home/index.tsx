f78b34d7a7d95b3b4f2e9007ec6696d39e03bf32
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-12-16 14:09:06 -0600

    feat: update HomePage to conditionally render menu items based on user roles

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,24 +1,29 @@
-import { Link } from "react-router-dom"
-import { MENU_ITEMS } from "./MENU_ITEMS"
+import { Link } from 'react-router-dom'
+import { MENU_ITEMS, MENU_ITEMS_SELLER } from './MENU_ITEMS'
+import { useUserInfo } from '../profile/hooks/use-user-info'
 
 export default function HomePage() {
+  const { userInfo } = useUserInfo()
+
+  const linkItems = userInfo?.roles.some((role) => role.name === 'admin')
+    ? MENU_ITEMS
+    : MENU_ITEMS_SELLER
+
   return (
     <div className="p-6 rounded flex items-center justify-center">
       <div className="grid grid-cols-2 sm:grid-cols-3 gap-8 sm:gap-16">
-        {
-          MENU_ITEMS.map((item, index) => {
-            return (
-              <Link
-                to={item.path}
-                className="p-4 shadow-lg rounded-lg hover:bg-gray-100 cursor-pointer flex flex-col gap-2 items-center"
-                key={index}
-              >
-                <item.icon width={30} className="text-gray-800" />
-                <span>{item.label}</span>
-              </Link>
-            )
-          })
-        }
+        {linkItems.map((item, index) => {
+          return (
+            <Link
+              to={item.path}
+              className="p-4 shadow-lg rounded-lg hover:bg-gray-100 cursor-pointer flex flex-col gap-2 items-center"
+              key={index}
+            >
+              <item.icon width={30} className="text-gray-800" />
+              <span>{item.label}</span>
+            </Link>
+          )
+        })}
       </div>
     </div>
   )
